1c65c2c7d69ae4e03939a2bdb78c890da3a6a1b4
CLINTON <issnasty2@gmail.com>
2024-05-07 14:23:43 +0300

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import logo from '../images/logo.png';
+export default function Navbar() {
+    return(
+        <div className="navbar">
+
+                    <div className="logo">
+                        <img className='logo' src={logo} alt="logo" />
+                    </div>
+
+                    <div className="searchbar">
+                        <input
+                        className='search' 
+                        type="text"
+                        placeholder="find car"
+                        />
+                    </div>           
+
+                    <div className="menu">
+                        <ul>
+                            <li><a href='#'>Home</a></li>
+                            <li><a href='#'>About</a></li>
+                            <li><a href='#'>Contact</a></li>
+                        </ul>
+                    </div>
+       </div>
+    )
+}
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,9 @@
+import logo from '../images/logo.png';
+
+export default function Welcome(){
+    return(
+        <>
+        
+        </>
+    )
+}
