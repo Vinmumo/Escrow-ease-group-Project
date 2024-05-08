952d6de697ecdb16e9840802fb7a4af96cf61076
CLINTON <issnasty2@gmail.com>
2024-05-08 11:56:37 +0300

    added routes

---
+++ src/Routes.js
@@ -0,0 +1,24 @@
+import app from './App';
+
+import App from "./App";
+// import BotData from "./botData";
+
+// import Transaction from "./components/Transaction";
+
+
+
+
+const routes = [
+   {
+    path: '/',
+    element: <App/>,
+    errorElement: <h1> An Error coccured</h1>
+   }
+   
+//    ,{
+//         path: "/bot-battle/botData/:id",
+//         element: <BotData/>
+//       }
+    ]
+
+export default routes
